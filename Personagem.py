1e7973fd87998a81d6956c9106583e21860dac77
Alexandre Conte <alexandreconte.dev@gmail.com>
2023-09-29 22:01:41 -0300

    implementada a classe personagem

--- Personagem.py
+++ Personagem.py
@@ -3,26 +3,36 @@ from AbstractPersonagem import *
 
 class Personagem(AbstractPersonagem):
     # Construtor fornecido, nao deve ser alterado
-    def __init__(self, energia: int, habilidade: int,
-                 velocidade: int, resistencia: int, tipo: Tipo):
-        pass  # implementar
+    def __init__(
+            self,
+            energia: int,
+            habilidade: int,
+            velocidade: int,
+            resistencia: int,
+            tipo: Tipo
+    ):
+        self.__energia = (None, energia)[isinstance(energia, int)]
+        self.__habilidade = (None, habilidade)[isinstance(habilidade, int)]
+        self.__velocidade = (None, velocidade)[isinstance(velocidade, int)]
+        self.__resistencia = (None, resistencia)[isinstance(resistencia, int)]
+        self.__tipo = (None, tipo)[isinstance(tipo, int)]
 
     @property
     def tipo(self) -> Tipo:
-        pass  # implementar
+        return self.__tipo
 
     @property
     def energia(self) -> int:
-        pass  # implementar
+        return self.__energia
 
     @property
     def habilidade(self) -> int:
-        pass  # implementar
+        return self.__habilidade
 
     @property
     def velocidade(self) -> int:
-        pass  # implementar
+        return self.__velocidade
 
     @property
     def resistencia(self) -> int:
-        pass  # implementar
+        return self.__resistencia
